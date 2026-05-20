278eafcd0dee8d816296ce02b2113bc2d0eb31d1
Aaron Zavora <zavoraad@gmail.com>
2026-05-20 12:44:21 -0400

    init

--- src/main/scala/com/databricks/industry/solutions/hl7v2ai/Field.scala
+++ src/main/scala/com/databricks/industry/solutions/hl7v2ai/Field.scala
@@ -1,8 +1,7 @@
 package com.databricks.industry.solutions.hl7v2ai
 
-import scala.collection.immutable.Vector
-
-final case class Field(components: Vector[Component], delim: Char) {
-  override def toString: String =
-    components.map(_.toString).mkString(delim.toString)
+final case class Field(components: ArraySeq[Component], delim: Char){
+    override def toString: String =
+      components.map(_.toString).mkString(delim.toString)
 }
+
